1682ab62d56a70c590c94e8674e35ace3b41815f
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2025-09-25 10:01:14 +0200

    Update and rename (NONFUCTIONAL)mirrorDetect.py to mirrorDetect.py
    now functional, changed detection from shape to colour

---
+++ mirrorDetect.py
@@ -0,0 +1,86 @@
+
+from detection import Detection
+import cv2 as cv
+import numpy as np
+
+class MirrorDetect(Detection):
+	def __init__ (self):
+
+		############
+		#HSV_values (subject to change)
+		###########
+		self.lr = np.array([144, 113, 0]) 
+		self.ur = np.array([180, 255, 255])
+		self.lr2 = np.array([170, 113, 0])
+		self.ur2 = np.array([180, 255, 255])
+
+		self.kernel = np.ones((5,5), np.uint8)
+		self.last_contours = []
+		self.frame = None
+	def __del__ (self):
+		pass
+	
+	def update_coordinates(self, coords):
+		self.coordinates = coords
+		coords.get_coordinates(coords)
+		return coords
+
+	def framegetter(self, frame):
+		if frame is None:
+			print("Failed to grab frame")
+			return None
+		self.frame = frame
+		return frame
+	
+	def findContour(self):
+		hsv = cv.cvtColor(self.frame, cv.COLOR_BGR2HSV)
+		mask = cv.inRange(hsv, self.lr, self.ur)
+		mask2 = cv.inRange(hsv, self.lr2, self.ur2)
+		mask = cv.bitwise_or(mask, mask2)
+    
+		mask = cv.morphologyEx(mask, cv.MORPH_OPEN, self.kernel)
+		mask = cv.morphologyEx(mask, cv.MORPH_CLOSE, self.kernel)
+    
+		contours, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+     
+		output = self.frame.copy()
+		
+		if contours:
+			smooth_cnt = []
+			mirror_cnts = sorted(contours, key=cv.contourArea, reverse=True)[:5]
+			for i, cnt in enumerate(mirror_cnts):
+				epsilon = 0.02 * cv.arcLength(cnt, True)
+				aprox = cv.approxPolyDP(cnt, epsilon, True)
+				prev = self.last_contours[i] if i < len(self.last_contours) else None
+				smooth = self.wrap_contour(prev, aprox, 0.5)
+				smooth_cnt.append(smooth)
+		
+		
+		self.last_contours = smooth_cnt
+		cv.drawContours(output, smooth_cnt, -1, (0, 255, 0), 2)
+		self.output_frame = output
+		self.mask_frame = mask
+		return output, mask
+	
+	def getCoordinates(self):
+		mid= []
+		for cnt in self.last_contours:
+			M_moments = cv.moments(cnt)
+			if M_moments['m00'] != 0:
+				centerx = int(M_moments['m10'] / M_moments['m00'])
+				centery = int(M_moments['m01'] / M_moments['m00'])
+				mid.append((centerx, centery))
+		
+		else:
+			mid.append(None)
+
+		print(mid)
+
+		return mid
+
+	def wrap_contour(self, prev, current, alpha=0.5):
+		if prev is None or len(prev) != len(current):
+			return current
+		return np.int32(prev * (1 - alpha) + current * alpha)
+
+	
