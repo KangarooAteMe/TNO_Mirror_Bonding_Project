893e2b3dc82913a11db34500097f38fd0647f01a
KangarooAteMe <144322903+KangarooAteMe@users.noreply.github.com>
2025-10-31 10:05:47 +0100

    Update Databuffer.py

--- Databuffer.py
+++ Databuffer.py
@@ -1,15 +1,32 @@
 from detection import detection
 
-class Databuffer:
+class databuffer:
     def __init__(self, mirror, block):
         self.mir = mirror
         self.blk = block
         self.arrayify = []
+        self.bl = 0
+        self.mi = 0
 
-    def collectVisData(self):
 
-        self.datablk = self.blk.getVals()
-        self.datamir = self.mir.getVals()
-        self.arrayify.append(self.datablk)
-        self.arrayify.append(self.datamir) ### not send correctly yet
-        print(self.arrayify) 
+    def collectVisData(self):    
+
+        datablk = self.blk.getVals(self.bl)
+        
+        if self.bl < len(self.blk.blocks):
+            
+            self.bl = self.bl + 1
+            return datablk
+        else:
+            return None
+
+    def collectfinalPos(self):    
+
+        pos = self.mir.getVals(self.mi)
+        
+        if self.mi < len(self.mir.mirrordata):
+            
+            self.mi = self.mi + 1
+            return pos
+        else:
+            return None
